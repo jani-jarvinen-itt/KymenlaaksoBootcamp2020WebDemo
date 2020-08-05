e4aeea3fd88331a5d08e43607acd46f6fa30e894
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-08-05 15:42:52 +0300

    Lisätty ottelujen satunnainen arpominen esimerkkinä Jannelle.

--- AspNetCoreBackend/AspNetCoreBackend/Controllers/OmaApiController.cs
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/OmaApiController.cs
@@ -17,6 +17,55 @@ namespace AspNetCoreBackend.Controllers
             return 123;
         }
 
+        [Route("satunnainen")]
+        public int Satunnaisluku()
+        {
+            Random random = new Random();
+            int luku = random.Next(1, 20);
+            return luku;
+        }
+
+        [Route("ottelut")]
+        public List<string> Ottelut()
+        {
+            string[] joukkueet = new string[]
+            {
+                "AAA", "BBB", "CCC", "DDD"
+            };
+
+            List<string> pelit = new List<string>();
+            Random random = new Random();
+
+            for (int laskuri = 0; laskuri < 12; laskuri++)
+            {
+                bool peliOk = false;
+                while (!peliOk)
+                {
+                    int indeksi = random.Next(0, joukkueet.Length);
+                    string kotiJoukkue = joukkueet[indeksi];
+
+                    bool indeksiOk = false;
+                    int indeksi2 = 0;
+                    while (!indeksiOk)
+                    {
+                        indeksi2 = random.Next(0, joukkueet.Length);
+                        indeksiOk = indeksi != indeksi2;
+                    }
+                    string vierasJoukkue = joukkueet[indeksi2];
+
+                    string peli = kotiJoukkue + "-" + vierasJoukkue;
+
+                    if (!pelit.Contains(peli))
+                    {
+                        pelit.Add(peli);
+                        peliOk = true;
+                    }
+                }
+            }
+
+            return pelit;
+        }
+
         [Route("merkkijono")]
         public string Merkkijono()
         {
