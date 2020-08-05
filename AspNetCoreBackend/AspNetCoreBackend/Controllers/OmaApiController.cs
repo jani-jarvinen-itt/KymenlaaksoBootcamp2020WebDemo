4327e05f160464e52aeb0b6275c6e72c196542f1
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-08-05 14:33:20 +0300

    Lisätty uusi API-kontrolleri sekä siihen luvun palauttaminen.

---
+++ AspNetCoreBackend/AspNetCoreBackend/Controllers/OmaApiController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreBackend.Controllers
+{
+    [Route("api/oma")]
+    [ApiController]
+    public class OmaApiController : ControllerBase
+    {
+        public int Luku()
+        {
+            return 123;
+        }
+    }
+}
