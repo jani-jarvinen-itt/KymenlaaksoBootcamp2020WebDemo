4054a9a58e81cafdcb04062cf08267dc521004ea
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-08-06 14:45:29 +0300

    Lisätty rakentaja (konstruktori) React-komponenttiin.

--- react-front/src/TodoKomponentti.tsx
+++ react-front/src/TodoKomponentti.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
-class TodoKomponentti extends React.Component {
+interface TodoTila {
+  otsikko: string;
+}
+
+class TodoKomponentti extends React.Component<{}, TodoTila> {
+
+  constructor(props: any) {
+    super(props);
+    this.state = { otsikko: "(tyhjä)" };
+  }
+
   render() {
-    return <h1>Tämä on Todo-komponentti</h1>;
+    return <div>
+      <h1>Tämä on Todo-komponentti</h1>
+      <p>Otsikko: {this.state.otsikko}</p>
+    </div>;
   }
 }
 
