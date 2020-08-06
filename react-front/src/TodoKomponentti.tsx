d3a577871798643c72386091b4ce1f1e8978f963
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-08-06 14:36:01 +0300

    Lisätty todo-komponentti Reactiin.

---
+++ react-front/src/TodoKomponentti.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class TodoKomponentti extends React.Component {
+  render() {
+    return <h1>Tämä on Todo-komponentti</h1>;
+  }
+}
+
+export default TodoKomponentti;
--- react-front/src/index.tsx
+++ react-front/src/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import TodoKomponentti from './TodoKomponentti';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TodoKomponentti />
   </React.StrictMode>,
   document.getElementById('root')
 );
