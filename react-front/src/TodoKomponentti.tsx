bae551ecdd8d58d60aea852912e1c853b1251a0f
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-08-06 15:01:34 +0300

    Lisätty Fetch-tuki JSONPlaceholder-web API:lle.

--- react-front/src/TodoKomponentti.tsx
+++ react-front/src/TodoKomponentti.tsx
@@ -11,6 +11,20 @@ class TodoKomponentti extends React.Component<{}, TodoTila> {
     this.state = { otsikko: "(tyhjä)" };
   }
 
+  componentDidMount() {
+
+    const reactKomponetti = this;
+
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => {
+        console.log("Saatiin vastaus JSON-palvelusta:");
+        console.log(json);
+
+        reactKomponetti.setState({ otsikko: json.title });
+      });
+  }
+
   render() {
     return <div>
       <h1>Tämä on Todo-komponentti</h1>
