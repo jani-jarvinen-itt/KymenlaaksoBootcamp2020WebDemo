ec3e724aaf6862b34d91d3a3b818869573a03e9c
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-08-10 13:31:26 +0300

    Lisätty peruskoodi tietokantayhteyden avaamiseen ja sulkemiseen.

--- AspNetCoreBackend/TietokantaDemoAdoNet/Program.cs
+++ AspNetCoreBackend/TietokantaDemoAdoNet/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,16 @@ namespace TietokantaDemoAdoNet
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Aloitetaan tietokantayhteyden avaus...");
+
+            string yhteysmerkkijono = "Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;";
+            SqlConnection yhteys = new SqlConnection(yhteysmerkkijono);
+            yhteys.Open();
+
+            Console.WriteLine("Tietokantayhtes avattu onnistuneesti.");
+
+            yhteys.Close();
+            Console.ReadLine();
         }
     }
 }
