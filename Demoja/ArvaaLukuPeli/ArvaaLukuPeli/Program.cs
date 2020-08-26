fd669b84463e5bd6a8846574e0cdc9f31af7a885
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-08-26 15:30:03 +0300

    Lisätty yksinkertainen pelologiikka.

--- Demoja/ArvaaLukuPeli/ArvaaLukuPeli/Program.cs
+++ Demoja/ArvaaLukuPeli/ArvaaLukuPeli/Program.cs
@@ -12,6 +12,22 @@ namespace ArvaaLukuPeli
             int oikeaVastaus = random.Next(1, 21);
             Console.WriteLine(oikeaVastaus);
 
+            Console.WriteLine("Anna arvauksesi välillä 1-20:");
+            string arvaus = Console.ReadLine();
+            int arvattuLuku = int.Parse(arvaus);
+
+            if (arvattuLuku < oikeaVastaus)
+            {
+                Console.WriteLine("Oikea luku on suurempi!");
+            }
+            else if (arvattuLuku > oikeaVastaus)
+            {
+                Console.WriteLine("Oikea luku on pienempi!");
+            }
+            else
+            {
+                Console.WriteLine("Oikein arvattu! Voit pelin!");
+            }
         }
     }
 }
