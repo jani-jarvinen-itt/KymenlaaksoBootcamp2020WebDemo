30f75800b530be4eb1ef27bddde1e037bdb13ecd
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-08-26 15:25:11 +0300

    Lisätty satunnaisen luvun arvonta.

--- Demoja/ArvaaLukuPeli/ArvaaLukuPeli/Program.cs
+++ Demoja/ArvaaLukuPeli/ArvaaLukuPeli/Program.cs
@@ -7,6 +7,11 @@ namespace ArvaaLukuPeli
         static void Main(string[] args)
         {
             Console.WriteLine("Tervetuloa peliin!");
+
+            Random random = new Random();
+            int oikeaVastaus = random.Next(1, 21);
+            Console.WriteLine(oikeaVastaus);
+
         }
     }
 }
