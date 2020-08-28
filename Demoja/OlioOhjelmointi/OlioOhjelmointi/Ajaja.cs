d51fc5fc2054ce0b14f5195f79a3f41195ab1880
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-08-28 14:47:57 +0300

    Lisätty Ajaja- ja Urheiluauto-luokat.

---
+++ Demoja/OlioOhjelmointi/OlioOhjelmointi/Ajaja.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OlioOhjelmointi
+{
+    class Ajaja
+    {
+        public string Nimi { get; set; }
+
+        public int Ikä { get; set; }
+
+        public bool OnAjokortti { get; set; }
+    }
+}
