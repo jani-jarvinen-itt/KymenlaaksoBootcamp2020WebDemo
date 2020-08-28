096a5ab438497f4d9de761ed9302dce14044e48e
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-08-28 11:58:51 +0300

    Lisätty Auto-luokan esimerkki.

---
+++ Demoja/OlioOhjelmointi/OlioOhjelmointi/Auto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OlioOhjelmointi
+{
+    class Auto
+    {
+        // metodit
+        void Käynnistä()
+        {
+        }
+
+        void Sammuta()
+        {
+        }
+
+        // ominaisuudet
+        string Väri { get; set; }
+
+        int Huippunopeus { get; set; }
+
+        public string Merkki { get; set; }
+    }
+}
