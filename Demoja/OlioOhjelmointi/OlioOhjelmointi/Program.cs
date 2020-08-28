55688e93999210b135367bb3f36340067df946f4
Jani Järvinen <jani.jarvinen@intertechno.org>
2020-08-28 11:38:28 +0300

    Aloitettu olio-ohjelmointidemon koodaaminen.

---
+++ Demoja/OlioOhjelmointi/OlioOhjelmointi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OlioOhjelmointi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
